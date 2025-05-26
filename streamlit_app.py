952a9c43f1d7df15d96aa2afc2ba8b2b323a7cf2
Diya Jibu <63443155+diyatj@users.noreply.github.com>
2025-05-26 16:31:33 -0500

    fixed a parantheses

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col,when_matched
-
+from snowflake.snowpark.functions import col, when_matched
+from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie!  :cup_with_straw:")
@@ -13,9 +13,8 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be: ', name_on_order)
 
-
 cnx = st.connection("snowflake")
-session = cnx.session(
+session = cnx.session  # fixed: removed the stray opening parenthesis
 
 session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
@@ -33,10 +32,11 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered! Name on order: {name_on_order} ✅ ')
+
